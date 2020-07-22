ead8f2dedf084fb0ea4367a38b8180e3ffb64473
Rene <rene.exe@gmail.com>
2020-07-22 00:04:15 -0300

    Criando atributos de dado para classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá'
     
 if __name__ == '__main__':
-    p = Pessoa
+    p = Pessoa('rene','33')
     print(Pessoa.cumprimentar(p))
-    print(id(p))
+    print(p.idade)
